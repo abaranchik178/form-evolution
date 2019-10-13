3cfeeb7c4a3958f0801927451a186c9b5e8d334a
Andrey B <abaranchik178@gmail.com>
2019-10-13 22:20:37 +0300

    add sql dump

---
+++ dump.sql
@@ -0,0 +1,11 @@
+CREATE TABLE users(
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) UNIQUE KEY,
+    first_name VARCHAR(70),
+    last_name VARCHAR(70),
+    gender enum('male', 'female') DEFAULT NULL,
+    birth_date DATE,
+    password_hash TINYTEXT NOT NULL,
+    secret_string TINYTEXT NOT NULL,
+    settings JSON DEFAULT NULL
+);
